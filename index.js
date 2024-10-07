4997a77e269c49d0ad41c1e776468cb33f40ab62
Mehadi Hossain <mehadi_hossain@yahoo.com>
2024-10-07 09:57:49 +0600

    Added course detatis and image fetch api

--- index.js
+++ index.js
@@ -12,10 +12,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/courses', (req, res) => {
-    res.send(courses)
-  })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+// Get a single course by ID
+app.get('/courses/:id', (req, res) => {
+  const courseId = req.params.id;
+  const course = courses.find(course => course.course_id === courseId);
+
+  if (course) {
+      res.json(course);
+  } else {
+      res.status(404).json({ message: 'Course not found' });
+  }
+});
+
+app.get('/images', (req, res) => {
+  // Extract all image URLs
+  const imageUrls = courses.map(course => {
+      return {
+          img_url: course.img_url
+      };
+  });
+  
+  res.json(imageUrls);
+});
